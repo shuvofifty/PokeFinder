54c1858c33ab4b382bf13bc1da305d53cb2c8e6e
Shubroto Shuvo <shubroto.debnath@gmail.com>
2021-08-01 16:09:10 -0500

    Initial Commit

---
+++ PokeFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeFinder
+//
+//  Created by Shubroto Shuvo on 8/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokeFinder/PokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeFinderApp.swift
+//  PokeFinder
+//
+//  Created by Shubroto Shuvo on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct PokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
